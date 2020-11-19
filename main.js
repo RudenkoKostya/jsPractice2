0423d1ca31ba221dc7afee67d913f3e8cc1a98ad
Kostyantyn <32463063+RudenkoKostya@users.noreply.github.com>
2020-11-19 11:34:29 +0200

    half work

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log('Hello, Array');
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr);
+//
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+
+// let arr = ['a', 'b', 'c', 'd'] ;
+//
+// let str = arr[0] + ' + ' + arr[1] + ' ,' + arr[2] + ' + ' + arr[3];
+// console.log(str);
+
+// let numArr = [2, 5, 3, 9];
+// let result = numArr[0] * numArr[1] + numArr[2] * numArr[3];
+// console.log(result);
+
+// let obj = {a: 1, b: 2, c: 3};
+//
+// console.log(obj['c']);
+// console.log(obj.c);
+// let PayObj = {Коля: '1000', Вася: '500', Петя: '200'};
+// console.log(PayObj.Коля);
+// console.log(PayObj.Петя);
+
